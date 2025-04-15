ef052f25133da3475d65f935bac863f1ac7d7aca
orcndogan <orcundogan@gmail.com>
2025-04-15 12:08:32 +0300

    Proje 2 tamamlandı.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,62 +2,67 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+        System.out.println(numberToWords(1548));
+
+
+
+
     }
 
-    public static String[] DIGIT_NAMES = {
-            "Zero",
-            "One",
-            "Two",
-            "Three",
-            "Four",
-            "Five",
-            "Six",
-            "Seven",
-            "Eight",
-            "Nine",
-    };
-    public  static boolean isPalindrome(int number) {
+    public static boolean isPalindrome(int number) {
+        int original = Math.abs(number);
+        int reversed = 0;
+        int referans = original;
 
-        String numberStr = Integer.toString(number);
-
-        String reverseNumberStr = new StringBuilder(numberStr).reverse().toString();
-
-        return numberStr.equals(reverseNumberStr);
+        while (referans > 0) {
+            int sonIndex = referans % 10;
+            reversed = (reversed * 10) + sonIndex;
+            referans /= 10;
+        }
+        return reversed == original;
     }
 
     public static boolean isPerfectNumber(int number){
-
-        int total = 0;
-
-        for (int i = 1;  i < number; i++){
-            if (number % i == 0)
-                total+= i;
+        int sayac=0;
+        if(number<=0)
+        {
+            return false;
+        }
+        for(int i=1;i<number;i++)
+        {
+            if(number %i==0)
+            {
+                sayac+=i;
+            }
         }
 
-        return total == number ;
+        return  number==sayac;
     }
 
-    public static String numbertoWords(int number){
-
-        if (number < 0)
+    public static String numberToWords(int number) {
+        if (number < 0) {
             return "Invalid Value";
-
-        String numberStr = Integer.toString(number);
-
-        String[] digits = new String[numberStr.length()];
-
-        for (int i = 0; i < numberStr.length(); i ++){
-
-            char digit = numberStr.charAt(i);
-
-            int x = Integer.parseInt(Character.toString(digit));
-
-            String digitStr = DIGIT_NAMES[x];
-
-            digits[i] = digitStr;
         }
-        return String.join(" ", digits);
-    }
 
+
+
+
+        String[] words = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+        StringBuilder result = new StringBuilder();
+        String numStr = Integer.toString(number);
+
+        for (int i = 0; i < numStr.length(); i++) {
+            for (int j = 0; j < words.length; j++) {
+                if (Character.getNumericValue(numStr.charAt(i)) == j) {
+                    result.append(words[j]).append(" ");
+                    break;
+                }
+            }
+        }
+
+        return result.toString().trim();
+    }
 }
